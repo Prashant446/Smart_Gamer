c339196fd3a8797a194e5b62b49b30f27d4f1795
Prashant446 <praskr@iitk.ac.in>
2019-05-29 14:44:10 +0530

    Added separate keyboard controlled version of game

--- behaviour_tree.py
+++ behaviour_tree.py
@@ -1,6 +1,53 @@
-import py_tree
+class TaskStatus:
+    FAILURE = 0
+    SUCCESS = 1
+    RUNNING = 2
 
-class Tree():
 
-    def __init__(self):
-        super().__init__()
+class task:
+
+    def __init__(self, name, children=None):
+        self.name = name
+        self.status = None
+        if children is None:
+            children = []
+        self.children = children
+
+    def run(self):
+        pass
+
+    def reset(self):
+        for c in self.children:
+            c.reset()
+
+    def insert_child(self, child, index):
+        self.children.insert(index, child)
+
+    def remove_child(self, c):
+        self.children.remove(c)
+
+    def prepend_child(self, c):
+        self.children.insert(0, c)
+
+    def add_child(self, c):
+        self.children.append(c)
+
+    def get_status(self):
+        return self.status
+
+    def set_status(self, s):
+        self.status = s
+
+    def announce(self):
+        print("Executing task " + str(self.name))
+
+    # These next two functions allow us to use the 'with' syntax
+    def __enter__(self):
+        return self.name
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            return False
+        return True
+
+
---
+++ game_V1.0.py
@@ -0,0 +1,259 @@
+import pygame as pyg
+import random
+
+# initialise pygames
+pyg.init()
+
+clock = pyg.time.Clock()
+
+# FONT -
+fontScore = pyg.font.SysFont('comicsans', 30, True, True)
+fontEnd = pyg.font.SysFont('comicsans', 70, True)
+
+winSize = (960, 810)
+sptSize = (128, 128)
+
+# SOUND EFFECTS -
+hitSound = pyg.mixer.Sound('resources/destroy.wav')
+fireSound = pyg.mixer.Sound('resources/shoot.wav')
+music = pyg.mixer.music.load('resources/music.mp3')
+pyg.mixer.music.play(-1)
+pyg.mixer.music.set_volume(0.5)
+
+# Window setup
+win = pyg.display.set_mode(winSize)
+bg = pyg.image.load('resources/Background/background1.jpg')
+pyg.display.set_caption('TheGame')
+
+
+# defining class of our ship
+class ship:
+    static = [pyg.transform.scale(pyg.image.load('resources/Blue/Small_ship_blue/L1.png'), sptSize),
+              pyg.transform.scale(pyg.image.load('resources/Blue/Small_ship_blue/R1.png'), sptSize)]
+    flyRight = [pyg.transform.scale(pyg.image.load('resources/Blue/Small_ship_blue/R2.png'), sptSize),
+                pyg.transform.scale(pyg.image.load('resources/Blue/Small_ship_blue/R3.png'), sptSize)]
+    flyLeft = [pyg.transform.scale(pyg.image.load('resources/Blue/Small_ship_blue/L2.png'), sptSize),
+               pyg.transform.scale(pyg.image.load('resources/Blue/Small_ship_blue/L3.png'), sptSize)]
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.dir = 0
+        self.vel = 15
+        self.width = 90     # not image width but the width of the rectangle around it
+        self.moveCount = 0  # to switch between sprites
+        self.health = 900
+
+    def draw(self, win):
+        if self.moveCount == 30:
+            self.moveCount = 0
+        else:
+            self.moveCount += 1
+
+        if self.dir == 0:
+            win.blit(self.static[self.moveCount % 2], (self.x, self.y))
+        elif self.dir == 1:
+            win.blit(self.flyRight[self.moveCount % 2], (self.x, self.y))
+        elif self.dir == -1:
+            win.blit(self.flyLeft[self.moveCount % 2], (self.x, self.y))
+        pyg.draw.rect(win, (0, 255, 0), (self.x + 15, self.y + self.width + 20, self.health/10, 10), 0)
+        pyg.draw.rect(win, (10, 255, 0), (self.x + 15, self.y + self.width + 20, self.width, 10), 1)
+
+
+class Asteroids:
+
+    def __init__(self, x, s):
+        self.x = x
+        self.y = -s
+        self.vel = 6 - (s-100)//50
+        self.width = s - 20
+        self.pop = False
+        self.size = (s, s)
+        self.health = s*2
+        self.img = pyg.transform.scale(pyg.image.load('resources/Aestroids/aestroid_brown.png'), self.size)
+
+    def motion(self):
+        if self.y < 810:
+            self.y += self.vel
+        else:
+            self.pop = True
+
+    def draw(self, win):
+        self.motion()
+        win.blit(self.img, (self.x, self.y))
+        # pyg.draw.rect(win, (255, 0, 0), (self.x+10, self.y, self.width, self.width), 2)
+
+
+class shots():
+    size = (48, 48)
+    image = pyg.transform.scale(pyg.image.load('resources/Blue/bullet.png'), size)
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vel = 16
+        self.pop = False
+        self.width = 20
+
+    def motion(self):
+        if self.y + 10 > 0:
+            self.y -= self.vel
+        else:
+            self.pop = True
+
+    def draw(self, win):
+        self.motion()
+        win.blit(self.image, (self.x, self.y))
+        # pyg.draw.rect(win, (255, 0, 0), (self.x + 14, self.y, self.width, self.width), 2)
+
+
+startLocn = (winSize[0]//2 - sptSize[0], winSize[1] - sptSize[1] - 10)
+player = ship(startLocn[0], startLocn[1])
+bullets = []
+asteroids = []
+
+
+def endWin():
+    win.blit(pyg.transform.scale(bg, winSize), (0, 0))
+    text1 = fontScore.render('GAME OVER', 2, (255, 0, 0))
+    text2 = fontScore.render('RESTART SOON', 2, (255, 0, 0))
+    win.blit(text1, (winSize[0]/2-100, winSize[1]/2-50))
+    win.blit(text2, (winSize[0]/2-110, winSize[1]/2))
+    pyg.display.update()
+
+
+def redrawWin(start, end):
+    win.blit(pyg.transform.scale(bg, winSize), (0, 0))
+    if start:
+        textStart = fontEnd.render('PRESS SPACE TO START', 2, (255, 0, 0))
+        win.blit(textStart, (winSize[0] / 2 - 320, winSize[1] / 2 - 50))
+    elif end:
+        text1 = fontEnd.render('GAME OVER', 2, (255, 0, 0))
+        text2 = fontEnd.render('RESTARTING SOON', 2, (255, 0, 0))
+        win.blit(text1, (winSize[0] / 2 - 200, winSize[1] / 2 - 50))
+        win.blit(text2, (winSize[0] / 2 - 250, winSize[1] / 2))
+    else:
+        text = fontScore.render('Score - ' + str(score), 2, (27, 229, 54))
+        win.blit(text, (winSize[0] - 150, winSize[1] - 50))
+    if not end:
+        if start:
+            player.dir = 0
+        player.draw(win)
+    for bullet in bullets:
+        if bullet.pop:
+            bullets.pop(bullets.index(bullet))
+        else:
+            bullet.draw(win)
+
+    for asteroid in asteroids:
+        if asteroid.pop:
+            asteroids.pop(asteroids.index(asteroid))
+        else:
+            asteroid.draw(win)
+
+    pyg.display.update()
+
+
+score = 0
+restartCounter = 100
+delayCounter = 25
+end = False
+start = True
+delay = False
+lastPosition = 100
+bulletInterval = 5
+asteroidInterval = 18
+play = True
+while play:
+    clock.tick(30)
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            play = False
+
+    for asteroid in asteroids:
+        if max(abs(asteroid.x + 10 + asteroid.width // 2 - player.x - 15 - player.width // 2),
+               abs(asteroid.y + asteroid.width // 2 -
+                   player.y - 10 - player.width // 2)) < ((asteroid.width + player.width) // 2 - 10):
+            # print('You hit an asteroid')
+            if not start and not end:
+                if player.health - 100 > 0:
+                    player.health -= 100
+                    asteroid.pop = True
+                else:
+                    player.health = 0
+
+        for bullet in bullets:
+            if bullet.y <= asteroid.y + asteroid.width - 5 and bullet.y > asteroid.y:
+                if bullet.x > asteroid.x - 5 and bullet.x < asteroid.x + asteroid.width - 5:
+                    bullet.pop = True
+                    asteroid.health -= 150
+                    score += 10
+        if asteroid.health < 0:
+            hitSound.play()
+            asteroid.pop = True
+
+    if asteroidInterval > 19:
+        asteroidInterval = 1
+        size = random.randint(100, 200)
+        pos_x = random.randint(0, 800)
+        if abs(lastPosition - pos_x) > 300 and (pos_x - size // 2) < winSize[0]:
+            lastPosition = pos_x
+            asteroids.append(Asteroids(pos_x, size))
+    else:
+        asteroidInterval += 1
+
+
+    if start:
+        keys = pyg.key.get_pressed()
+        if keys[pyg.K_SPACE]:
+            delay = True
+        if delay:
+            if delayCounter > 0:
+                pyg.time.delay(10)
+                delayCounter -= 1
+                for event in pyg.event.get():
+                    if event.type == pyg.QUIT:
+                        pyg.quit()
+            else:
+                delayCounter = 25
+                start = False
+                delay = False
+
+    elif not end:
+
+        if player.health == 0:
+            end = True
+
+        keys = pyg.key.get_pressed()
+
+        if keys[pyg.K_RIGHT] and player.x + player.width < winSize[0]:
+            player.dir = 1
+            player.x += player.vel
+        elif keys[pyg.K_LEFT] and player.x > 0:
+            player.dir = -1
+            player.x -= player.vel
+        else:
+            player.dir = 0
+
+        if keys[pyg.K_SPACE]:
+            if bulletInterval > 4:
+                fireSound.play()
+                bulletInterval = 0
+                bullets.append(shots(player.x + (player.width//2), player.y))
+            else:
+                bulletInterval += 1
+
+    else:
+        score = 0
+        pyg.time.delay(10)
+        restartCounter -= 1
+        if restartCounter == 0:
+            restartCounter = 100
+            player.health = 1000
+            end = False
+            start = True
+
+    redrawWin(start, end)
+
+
+
